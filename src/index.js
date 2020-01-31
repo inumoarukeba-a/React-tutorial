b8e142b5a9e144e95f2b1e9ff147e0da547a453e
Ataru Nakano <atarunakano@gmail.com>
2020-01-31 09:19:54 +0900

    stateの値を変更する関数

--- src/index.js
+++ src/index.js
@@ -3,8 +3,24 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      value: null
+    };
+  }
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button
+        className="square"
+        onClick={() => {
+          this.setState({ value: "x" });
+        }}
+      >
+        {this.state.value}
+      </button>
+    );
   }
 }
 
